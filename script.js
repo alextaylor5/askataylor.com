d22ece1dccbde5a8aee240c277de13bf3b155bcc
AlexTaylor5 <alextaylor1@thetaylorplace.com>
2025-06-01 16:44:25 -0700

    Moved entire website to askataylor

--- script.js
+++ script.js
@@ -1,2 +1,64 @@
 console.log ("This is my first line of java script I have written")
 console.log ("This is the second line of java script I have written")
+
+function displayTime(){
+    var dateTime = new Date();
+    var hrs = dateTime.getHours();
+    var min = dateTime.getMinutes();
+    var sec = dateTime.getSeconds();
+    var session = document.getElementById('session');
+
+    if(hrs >= 12){
+        session.innerHTML = 'PM';
+    }else{
+        session.innerHTML = 'AM';
+    }
+
+    if(hrs > 12){
+        hrs = hrs - 12;
+    }
+
+    document.getElementById('hours').innerHTML = hrs;
+    document.getElementById('minutes').innerHTML = min;
+    document.getElementById('seconds').innerHTML = sec;
+}
+setInterval(displayTime, 10);
+/* navigation */
+
+function clickFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  
+    var x = document.getElementById("myLinks1");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+    
+    var x = document.getElementById("myLinks2");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+    
+    var x = document.getElementById("myLinks3");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+    
+    var x = document.getElementById("myLinks4");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  /* end of navigation */
